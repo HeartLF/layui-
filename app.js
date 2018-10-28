ff97c93bef2a0ac4b95ae0b4a7a7c757a1f65366
HeartLF <1061256268@qq.com>
2018-10-28 10:50:53 +0800

    layui博客管理

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var request = require('request');
 app.use(express.static('./public'));
 app.use('/', (req, res) => {
-    var url = 'http://192.168.1.108' + req.url;
+    var url = 'http://192.168.1.115' + req.url;
     req.pipe(request(url)).pipe(res);
 })
 
